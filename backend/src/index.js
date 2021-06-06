5aff1eafd018b85eaa8c5fe12cac272a37d9278b
ttales gold roger <ttalessoft@gmail.com>
2021-06-06 15:48:12 -0300

    mongo setup - ok

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+/**
+ * database setup
+ */
+
+mongoose.connect('mongodb://localhost:27017/upload', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.use(require('./routes'));
+
+app.listen(3000);
