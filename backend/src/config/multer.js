643045e49ff781b93c9218a7119155f3b0aa129c
ttales gold roger <ttalessoft@gmail.com>
2021-06-07 09:20:42 -0300

    backend - ok

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -23,7 +23,7 @@ const storageTypes = {
   }),
   s3: multerS3({
     s3: new aws.S3(),
-    bucket: 'cdn-file-upload',
+    bucket: process.env.AWS_BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: 'public-read',
     key: (req, file, cb) => {
@@ -42,7 +42,7 @@ const storageTypes = {
 
 module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: storageTypes['local'],
+  storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
